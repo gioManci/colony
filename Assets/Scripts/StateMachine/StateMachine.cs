4c1b664f7efbaa3ef23d28d82bcb6de2645e2bc3
TheVerga <matteo4.vergani@polimi.it>
2015-11-17 10:29:30 +0100

    Aged class and basic State Machine and specialization

---
+++ Assets/Scripts/StateMachine/StateMachine.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StateMachine : MonoBehaviour
+{
+    public State startingState;
+
+    private State currentState;
+    private State previousState;
+
+    void Start()
+    {
+        currentState = startingState;
+    }
+
+	void Update ()
+	{
+        currentState?.OnUpdate(gameObject);
+	}
+
+    public void ChangeToState(State newState)
+    {
+        previousState = currentState;
+        currentState.OnExit(gameObject);
+        currentState = newState;
+        currentState?.OnEnter(gameObject);
+    }
+
+    public void RevertToPreviousState()
+    {
+        ChangeToState(previousState);
+    }
+}
---
+++ Assets/Scripts/StateMachine/States/State.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class State
+{
+    public abstract void OnEnter(GameObject gameObject);
+    public abstract void OnUpdate(GameObject gameObject);
+    public abstract void OnExit(GameObject gameObject);
+}
