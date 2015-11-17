b378cb3983cd797ced7fc89103d696e78bc6dca7
TheVerga <matteo4.vergani@polimi.it>
2015-11-17 10:29:30 +0100

    Aged class and basic State Machine and specialization

---
+++ Assets/Scripts/Aged.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Aged : MonoBehaviour
+{
+
+    public float startingAge;
+    public float Age { private set; get; }
+	
+    void Start()
+    {
+        Age = startingAge;
+    }
+
+	void Update ()
+	{
+        Age -= Time.deltaTime;
+        if (Age < 0)
+        {
+            // Add code to handle destruction
+            Destroy(gameObject);
+        }
+	}
+}
---
+++ Assets/Scripts/StateMachine/States/Harvest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Harvest : State
+{
+    public override void OnEnter(GameObject gameObject)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void OnExit(GameObject gameObject)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void OnUpdate(GameObject gameObject)
+    {
+        throw new NotImplementedException();
+    }
+}
