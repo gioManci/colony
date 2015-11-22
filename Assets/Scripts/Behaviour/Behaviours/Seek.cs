14d97757a729870a9e9d633de1785aa51052c72b
TheVerga <matteo4.vergani@polimi.it>
2015-11-22 16:45:23 +0100

    Tidied up the code.

--- Assets/Scripts/Behaviour/Behaviours/Seek.cs
+++ Assets/Scripts/Behaviour/Behaviours/Seek.cs
@@ -5,20 +5,20 @@ using System;
 public class Seek : Behaviour
 {
     private Vector2 target;
+    private Rigidbody2D rigidbody;
 
     public Seek(GameObject actor, Vector2 target) : base(actor, BehaviourType.Seek)
     {
         this.target = target;
+        rigidbody = actor.GetComponent<Rigidbody2D>();
     }
 
     public override Vector2 Compute()
     {
-        Rigidbody2D rigidbody = actor.GetComponent<Rigidbody2D>();
         Vector2 position = actor.transform.position;
-        Vector2 distance = target - position;
-        Vector2 normalized = distance.normalized;
+        Vector2 distanceVector = target - position;
         //TODO: 1.0f is the max speed. It must be substituted with a constant.
-        Vector2 desiredVelocity = normalized * 1.0f;
+        Vector2 desiredVelocity = distanceVector.normalized * 2.0f;
         return desiredVelocity - rigidbody.velocity;
     }
 }
