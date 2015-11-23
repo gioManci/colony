14f58f6313826682480bc6c819e34dd1d75847f5
silverweed <silverweed1991@gmail.com>
2015-11-23 17:10:28 +0100

    add Resources

---
+++ Assets/Scripts/Resources/Flower.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Colony.Resources {
+
+public class Flower : ResourceYielder {
+	public Flower() : base(new ResourceSet()
+				.With(ResourceType.Nectar, 25)
+				.With(ResourceType.Pollen, 25) 
+				.With(ResourceType.Water, 25)
+				) {}
+}
+
+}
---
+++ Assets/Scripts/Resources/ResourceSet.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+namespace Colony.Resources {
+
+public class ResourceSet {
+	public ResourceSet() {}
+	
+	public ResourceSet(ResourceSet other) {
+		for (int i = 0; i < other.Length; ++i)
+			resources[i] = other[i];
+	}
+
+	public ResourceSet With(ResourceType type, int amount) {
+		resources[(int)type] += amount;
+		return this;
+	}
+	
+	public static ResourceSet operator -(ResourceSet rs, int amt) {
+		var newrs = new ResourceSet(rs);
+		for (int i = 0; i < newrs.Length; ++i) {
+			newrs[i] = Mathf.Max(0, newrs[i] - amt);
+		}
+		return newrs;
+	}
+	
+	public int Length {
+		get { return resources.Length; }
+	}
+	
+	public int this[int i] {
+		get { return resources[i]; }
+		set { resources[i] = value; }
+	}
+
+	private int[] resources = new int[Enum.GetNames(typeof(ResourceType)).Length];
+}
+
+}
