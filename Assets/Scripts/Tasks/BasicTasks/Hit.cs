3c3d48b345fa8e857f31e12e82c1a6387390c193
TheVerga <matteo4.vergani@polimi.it>
2015-12-11 14:22:28 +0100

    Addes some classes i forgot to add in last commit.

---
+++ Assets/Scripts/Tasks/BasicTasks/Hit.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+
+namespace Colony.Tasks.BasicTasks
+{
+    public class Hit : Task
+    {
+        private GameObject target;
+        private Stats stats;
+
+        public Hit(GameObject agent, GameObject target) : base(agent, TaskType.Hit)
+        {
+            this.target = target;
+            stats = agent.GetComponent<Stats>();
+        }
+
+        public override void Activate()
+        {
+            status = Status.Active;
+        }
+
+        public override void OnMessage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Status Process()
+        {
+            ActivateIfInactive();
+
+            //TODO: Substitute constant
+            if (target == null || Vector2.Distance(agent.transform.position, target.transform.position) > 1.0f)
+            {
+                status = Status.Failed;
+            }
+            else
+            {
+                //TODO: Get the enemy's battling system component and notify it of the hit.
+                Life life = target.GetComponent<Life>();
+                life.Decrease(stats.Damage);
+                status = Status.Completed;
+            }
+
+            return status;
+        }
+
+        public override void Terminate()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Tasks/ComplexTasks/Attack.cs
@@ -0,0 +1,68 @@
+﻿using Colony.Tasks.BasicTasks;
+using System;
+using UnityEngine;
+namespace Colony.Tasks.ComplexTasks
+{
+    public class Attack : ComplexTask
+    {
+        private GameObject enemy;
+
+        public Attack(GameObject agent, GameObject enemy) : base(agent, TaskType.Attack)
+        {
+            this.enemy = enemy;
+        }
+
+        public override void Activate()
+        {
+            status = Status.Active;
+            RemoveAllSubtasks();
+
+            AddSubtask(new Hit(agent, enemy));
+            AddSubtask(new Move(agent, enemy.transform.position, 1.0f));
+        }
+
+        public override void OnMessage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Status Process()
+        {
+            if (enemy == null)
+            {
+                status = Status.Completed;
+            }
+            else
+            {
+                ActivateIfInactive();
+                Status subtasksStatus = ProcessSubtasks();
+                if (subtasksStatus == Status.Completed)
+                {
+                    status = Status.Inactive;
+                }
+            }
+
+            return status;
+        }
+
+        public override void Terminate()
+        {
+            
+        }
+
+        private void HandleFailure()
+        {
+            if (subtasks.Peek().Type == TaskType.Hit)
+            {
+                if (enemy == null)
+                {
+                    status = Status.Completed;
+                }
+                else if (Vector2.Distance(agent.transform.position, enemy.transform.position) > 1.0f)
+                {
+
+                }
+            }
+        }
+    }
+}
