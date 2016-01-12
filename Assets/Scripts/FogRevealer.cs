c037d1dca49f9a6d40c3d389b0bdfc58f8a45afd
silverweed <silverweed1991@gmail.com>
2016-01-12 18:25:57 +0100

    start adding Fog of War

---
+++ Assets/Scripts/FogRevealer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// An unit which unravels the Fog of War
+public class FogRevealer : MonoBehaviour {
+
+	public float RevealRadius = 5f;
+
+	// Update is called once per frame
+	void Update() {
+		
+	}
+}
